584881f40ce2fd46fcbebb961d56424e94cfe0b6
dachan <dachankim@parastar.co.kr>
2026-09-01 16:44:47 +0900

    feat: global header 추가

--- src/components/GlobalHeader.jsx
+++ src/components/GlobalHeader.jsx
@@ -1,5 +1,27 @@
+import logo from '@/assets/logo.svg';
+import styles from '@/components/GlobalHeader.module.css';
+import { Link, useNavigate } from 'react-router';
+
 function GlobalHeader() {
-  return <div>global header</div>;
+  const navigate = useNavigate();
+
+  const handleMakeStudy = () => {
+    //  TODO: 스터디 만들기 페이지 경로 필요
+    navigate('/');
+  };
+
+  return (
+    <nav className={styles.container}>
+      <div className={styles.wrapper}>
+        <Link to="/">
+          <img src={logo} />
+        </Link>
+        <button onClick={handleMakeStudy}>
+          <span>스터디 만들기</span>
+        </button>
+      </div>
+    </nav>
+  );
 }
 
 export default GlobalHeader;
