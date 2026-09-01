356731667dd2d0074ffc7ee6d694dbd787e584e2
YANGPA <wodud7273@gmail.com>
2026-09-02 02:10:28 +0900

    feat: 오늘의 습관 페이지 FE 제작

--- src/pages/HabitPage/HabitPage.jsx
+++ src/pages/HabitPage/HabitPage.jsx
@@ -1,29 +1,43 @@
 import styles from './HabitPage.module.css';
+import icArrowRight from '/src/assets/ic_arrow_right.svg'
 
 function HabitPage(){
+
+ const timeNow = new Date().toLocaleString('sv-SE', { timeZone: 'Asia/Seoul' }).replace(' ', ' ');
+
 return (
 <>
 <div className={styles.habitPageDiv}>
   <div className={styles.habitInnerDiv}>
     <div className={styles.habitHead}>
       <div className={styles.habitTitle}>
-  <h1>연우의 개발공장</h1> <button>오늘의 집중</button><button>홈</button>
+  <h1>연우의 개발공장</h1> 
+  <div className={styles.habitTitleButtonDiv}>
+    <button className={styles.todayFocusButton}>오늘의 집중
+       <img src={icArrowRight} alt='오른쪽 화살표'></img>
+    </button>
+    <button className={styles.homeButton}>홈
+      <img src={icArrowRight} alt='오른쪽 화살표'></img>
+    </button>
+    </div>
   </div>
-  <div className ={styles.nowTime}>
-    <p>현재시간</p>
-    <div className ={styles.nowTimeBox}>
-    <p>시간</p>
+  <div className ={styles.nowTimeDiv}>
+    <p className={styles.nowTimeP}>현재시간</p>
+    <div className ={styles.timeBox}>
+    <p className ={styles.time}>{timeNow}</p>
     </div>
      </div>
        </div>
     <div className = {styles.todayHabitDiv}>
       <div className ={styles.todayHabitInnerDiv}>
         <div className={styles.todayHabitTitle}>
-          <p>오늘의 습관</p> <button>목록 수정</button>
+          <p className={styles.todayHabitP}>오늘의 습관</p>
+           <p className={styles.listModifyP}>목록 수정</p>
+            </div>
         <div className={styles.todayHabbitBoard}>
           <p>아직 습관이 없어요<br/> 목록 수정을 눌러 습관을 생성해보세요</p>
         </div>
-        </div>
+       
       </div>
 
     </div>
