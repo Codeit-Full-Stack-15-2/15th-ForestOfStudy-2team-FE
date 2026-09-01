a589bc6350485092492d27f8ca7fb9d77be1f3a0
dachan <dachankim@parastar.co.kr>
2026-09-01 17:04:00 +0900

    fix: 전체 배경화면 컬러 세팅

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,5 @@
-import { Link } from 'react-router';
-import styles from './Home.module.css';
-
 function Home() {
-  return (
-    <div className={styles.test_class} style={{ fontFamily: 'JejuStoneWall' }}>
-      홈<Link to="study-detail">스터디 페이지</Link>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
