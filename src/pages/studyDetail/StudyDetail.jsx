34cba30d6a8c890ccc5a22cd9451edb31c181394
dachan <dachankim@parastar.co.kr>
2026-09-01 17:28:49 +0900

    feat: 페이지 레이아웃 세팅

--- src/pages/studyDetail/StudyDetail.jsx
+++ src/pages/studyDetail/StudyDetail.jsx
@@ -1,4 +1,6 @@
+import styles from '@/pages/studyDetail/StudyDetail.module.css';
+
 function StudyDetail() {
-  return <div>study detail</div>;
+  return <section className={styles.container}>연우의 개발공장</section>;
 }
 export default StudyDetail;
