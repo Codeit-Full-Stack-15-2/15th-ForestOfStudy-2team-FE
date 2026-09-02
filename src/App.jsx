7016edd84005d7c4170faf5e696eeb177705004f
SoCheol Kwak <thcjfd@naver.com>
2026-09-02 20:39:42 +0900

    Fix routing and uncomment Home component
    Uncommented the Home component import and fixed typos in Route elements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 import Layout from './components/Layout';
-//import Home from './pages/home/Home';
+import Home from './pages/home/Home';
 import HabitPage from './pages/HabitPage/HabitPage';
 import NotFound from './pages/notFound/NotFound';
 import StudyDetail from './pages/studyDetail/StudyDetail';
@@ -13,13 +13,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="studies/new" element={<StudyCreate />} />
+          {/* <Route path="studies/new" element={<StudyCreate />} /> */}
           <Route path="studies/:studyId" element={<StudyDetail />} />
-          <Route path="studies/:studyId/edit" element={<StudyEdit />} />
+          {/* <Route path="studies/:studyId/edit" element={<StudyEdit />} /> */}
           <Route path="studies/:studyId/habits" element={<HabitPage />} />
           <Route path="studies/:studyId/focus" element={<FocusPage />} />
         </Route> 
-        <Route path="*" elemet={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
