adb8914937e250be2d5a1cc0ce707770ee77f11b
SoCheol Kwak <thcjfd@naver.com>
2026-09-04 14:14:12 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { Route, Routes } from 'react-router';
 import './App.css';
 import Layout from '@/components/Layout';
 import Home from '@/pages/home/Home';
-import HabitPage from '@/pages/HabitPage/HabitPage';
-import NotFound from '@/pages/notFound/NotFound';
+import StudyDetail from '@/pages/studyCreate/StudyCreate';
 import StudyDetail from '@/pages/studyDetail/StudyDetail';
+import StudyDetail from '@/pages/studyEdit/StudyEdit';
+import HabitPage from '@/pages/HabitPage/HabitPage';
+import StudyDetail from '@/pages/focusPage/FocusPage';
+import NotFound from '@/pages/notFound/NotFound';
 import './reset.css';
 
 function App() {
