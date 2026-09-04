fd1f1d1ee08e0fb31369525573e4a88003a01e42
dachan <dachankim@parastar.co.kr>
2026-09-04 14:28:18 +0900

    chore: main merge

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import HabitPage from '@/pages/HabitPage/HabitPage';
 import FocusPage from '@/pages/focusPage/FocusPage';
 import Home from '@/pages/home/Home';
 import NotFound from '@/pages/notFound/NotFound';
+import StudyCreate from '@/pages/studyCreate/StudyCreate';
 import StudyDetail from '@/pages/studyDetail/StudyDetail';
+import StudyEdit from '@/pages/studyEdit/StudyEdit';
 import { Route, Routes } from 'react-router';
 import './App.css';
 import './reset.css';
@@ -14,7 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="studies/new" element={<StudyCreate />} />
           <Route path="studies/:studyId" element={<StudyDetail />} />
+          <Route path="studies/:studyId/edit" element={<StudyEdit />} />
           <Route path="studies/:studyId/habits" element={<HabitPage />} />
           <Route path="studies/:studyId/focus" element={<FocusPage />} />
         </Route>
