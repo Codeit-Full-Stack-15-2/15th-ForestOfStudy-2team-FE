13bc87ff3618f64cf03d895bd1311b0fd6654284
dachan <dachankim@parastar.co.kr>
2026-09-05 18:28:00 +0900

    chore: 스터디 상세 페이지 헤더 영역 목업 데이터 연결

--- src/pages/studyDetail/components/StudyDetailHeader.jsx
+++ src/pages/studyDetail/components/StudyDetailHeader.jsx
@@ -5,7 +5,7 @@ import StudyActions from './StudyActions';
 import styles from './StudyDetailHeader.module.css';
 import StudyReactions from './StudyReactions';
 
-function StudyDetailHeader() {
+function StudyDetailHeader({ data }) {
   const { handleStudyShare, handleStudyEdit, handleStudyRemove } =
     useStudyActions();
 
@@ -28,11 +28,11 @@ function StudyDetailHeader() {
           />
         </div>
         <div className={styles.reaction}>
-          <StudyReactions />
+          <StudyReactions reactions={data.reactions} />
         </div>
       </div>
       <div className={styles.titleContainer}>
-        <h2 className={styles.title}>연우의 개발공장</h2>
+        <h2 className={styles.title}>{data.title}</h2>
         <div className={styles.titleButtons}>
           <ArrowButton onClick={handleNavigateToHabits}>
             습관 달성 기록하기
@@ -42,15 +42,13 @@ function StudyDetailHeader() {
       </div>
       <div className={styles.descriptionContainer}>
         <p className={styles.label}>소개</p>
-        <p className={styles.description}>
-          Slow And Steady Wins The Race! 다들 오늘 하루도 화이팅 :)
-        </p>
+        <p className={styles.description}>{data.description}</p>
       </div>
       <div className={styles.pointContainer}>
         <p className={styles.label}>현재까지 획득한 포인트</p>
         <div className={styles.badge}>
           <img src={point} alt="포인트 아이콘" />
-          <span>310P 획득</span>
+          <span>{data.totalPoints}&nbsp;획득</span>
         </div>
       </div>
     </section>
--- src/pages/studyDetail/components/StudyReactions.jsx
+++ src/pages/studyDetail/components/StudyReactions.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import ReactionBadge from './ReactionBadge';
 import styles from './StudyReactions.module.css';
 
-function StudyReactions() {
+function StudyReactions({ reactions }) {
   const [isPickerOpen, setIsPickerOpen] = useState(false);
   const [isAllBadgeOpen, setIsAllBadgeOpen] = useState(false);
 
@@ -28,18 +28,8 @@ function StudyReactions() {
   return (
     <div className={styles.container}>
       <div className={styles.badges}>
-        {[
-          { id: 1, emoji: '👱‍♀️', count: 37 },
-          { id: 2, emoji: '👍🏻', count: 50 },
-          { id: 3, emoji: '🤩', count: 50 },
-          // 더보기(+5..) 클릭 시 펼쳐질 추가 이모지들
-          { id: 4, emoji: '🔥', count: 12 },
-          { id: 5, emoji: '🎉', count: 8 },
-          { id: 6, emoji: '💻', count: 15 },
-          { id: 7, emoji: '☕', count: 20 },
-          { id: 8, emoji: '💪', count: 9 },
-        ].map((i) => (
-          <ReactionBadge emoji={i.emoji} count={i.count} />
+        {reactions.map((reaction) => (
+          <ReactionBadge emoji={reaction.emoji} count={reaction.count} />
         ))}
         <button
           className={`${styles.reactionBadge} ${styles.more}`}
@@ -49,18 +39,8 @@ function StudyReactions() {
         </button>
         {isAllBadgeOpen && (
           <div className={styles.allReactions}>
-            {[
-              { id: 1, emoji: '👱‍♀️', count: 37 },
-              { id: 2, emoji: '👍🏻', count: 50 },
-              { id: 3, emoji: '🤩', count: 50 },
-              // 더보기(+5..) 클릭 시 펼쳐질 추가 이모지들
-              { id: 4, emoji: '🔥', count: 12 },
-              { id: 5, emoji: '🎉', count: 8 },
-              { id: 6, emoji: '💻', count: 15 },
-              { id: 7, emoji: '☕', count: 20 },
-              { id: 8, emoji: '💪', count: 9 },
-            ].map((i) => (
-              <ReactionBadge emoji={i.emoji} count={i.count} />
+            {reactions.map((reaction) => (
+              <ReactionBadge emoji={reaction.emoji} count={reaction.count} />
             ))}
           </div>
         )}
