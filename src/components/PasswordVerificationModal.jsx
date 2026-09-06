11cf5df60578914770294ff6e5cf1a0b54e257da
dachan <dachankim@parastar.co.kr>
2026-09-06 09:32:44 +0900

    feat: 모달 컴포넌트 디테일링

--- src/components/PasswordVerificationModal.jsx
+++ src/components/PasswordVerificationModal.jsx
@@ -1,13 +1,21 @@
 import Button from './Button';
 import styles from './PasswordVerificationModal.module.css';
 
-function PasswordVerificationModal() {
+function PasswordVerificationModal({
+  open = true,
+  title,
+  description,
+  onOk,
+  onCancel,
+}) {
+  if (!open) return null;
+
   return (
     <div className={styles.container}>
       <div className={styles.modalContainer}>
         <div className={styles.modalHeader}>
-          <p className={styles.title}>연우의 개발공장</p>
-          <p className={styles.description}>권한이 필요해요!</p>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.description}>{description}</p>
         </div>
         <div className={styles.modalBody}>
           <div className={styles.inputContainer}>
@@ -16,9 +24,11 @@ function PasswordVerificationModal() {
           </div>
         </div>
         <div className={styles.modalFooter}>
-          <Button fullWidth={true}>수정하러 가기</Button>
+          <Button onClick={onOk} fullWidth={true}>
+            수정하러 가기
+          </Button>
           <div className={styles.textButtonContainer}>
-            <Button variant="text" size="small">
+            <Button onClick={onCancel} variant="text" size="small">
               나가기
             </Button>
           </div>
