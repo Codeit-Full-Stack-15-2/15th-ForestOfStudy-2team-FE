16469ae9040f900784be70e18985bb0c183e0c21
dachan <dachankim@parastar.co.kr>
2026-09-06 15:13:16 +0900

    feat: 리액션 아이콘 기본 로직 추가

--- src/pages/studyDetail/components/StudyReactions.jsx
+++ src/pages/studyDetail/components/StudyReactions.jsx
@@ -1,4 +1,5 @@
 import smile from '@/assets/ic_smile.svg';
+import { getOrCreateUserId } from '@/utils/userAuth';
 import data from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
 import { useEffect, useRef, useState } from 'react';
@@ -10,15 +11,32 @@ const VISIBLE_LIMIT = 3;
 function StudyReactions({ studyId, initialReactions }) {
   const [isOpenEmojiPicker, setIsOpenEmojiPicker] = useState(false);
   const [isOpenReactionList, setIsOpenReactionList] = useState(false);
+  const [reactions, setReactions] = useState(initialReactions);
+  const currentUserId = getOrCreateUserId();
+
   const reactionListPopoverRef = useRef(null);
   const emojiPickerRef = useRef(null);
 
   const handleSelectEmoji = (emoji) => {
-    console.log('선택된 이모지:', emoji.native);
+    const selectedEmoji = emoji.native;
+
+    setReactions((prev) => {
+      const exist = prev.find((item) => item.emoji === selectedEmoji);
+      if (exist) {
+        return prev.map((item) =>
+          item.emoji === selectedEmoji
+            ? { ...item, count: item.count + 1 }
+            : item,
+        );
+      }
+      return [...prev, { emoji: selectedEmoji, count: 1, reactedUserIds: [] }];
+    });
+
     // 활성화된 내부 포커스를 해제하여 부모 숨김 시 충돌 방지
     if (document.activeElement instanceof HTMLElement) {
       document.activeElement.blur();
     }
+    setIsOpenEmojiPicker(false);
   };
 
   const handleToggleEmojiPicker = () => {
@@ -53,22 +71,24 @@ function StudyReactions({ studyId, initialReactions }) {
   return (
     <div className={styles.container}>
       <div className={styles.badges}>
-        {initialReactions.slice(0, VISIBLE_LIMIT).map((reaction) => (
+        {reactions.slice(0, VISIBLE_LIMIT).map((reaction) => (
           <ReactionBadge
             key={reaction.emoji}
             emoji={reaction.emoji}
             count={reaction.count}
           />
         ))}
-        <button
-          className={`${styles.reactionBadge} ${styles.more}`}
-          onClick={handleToggleReactionList}
-        >
-          + {initialReactions.length - VISIBLE_LIMIT}..
-        </button>
+        {reactions.length > VISIBLE_LIMIT && (
+          <button
+            className={`${styles.reactionBadge} ${styles.more}`}
+            onClick={handleToggleReactionList}
+          >
+            + {reactions.length - VISIBLE_LIMIT}..
+          </button>
+        )}
         {isOpenReactionList && (
           <div ref={reactionListPopoverRef} className={styles.allReactions}>
-            {initialReactions.map((reaction) => (
+            {reactions.map((reaction) => (
               <ReactionBadge
                 key={reaction.emoji}
                 emoji={reaction.emoji}
