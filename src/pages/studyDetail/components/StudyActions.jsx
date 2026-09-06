caf91059a7be77d76e705d10740054e06817bdf4
dachan <dachankim@parastar.co.kr>
2026-09-07 02:33:44 +0900

    fix: 텍스트 버튼 컴포넌트로 교체

--- src/pages/studyDetail/components/StudyActions.jsx
+++ src/pages/studyDetail/components/StudyActions.jsx
@@ -1,11 +1,17 @@
+import Button from '@/components/Button';
 import styles from './StudyActions.module.css';
 
 function StudyActions({ onShare, onEdit, onRemove }) {
   return (
     <div className={styles.container}>
-      <button onClick={onShare}>공유하기</button>|
-      <button onClick={onEdit}>수정하기</button>|
-      <button onClick={onRemove}>스터디 삭제하기</button>
+      <Button variant="text" size="none" onClick={onShare}>
+        공유하기
+      </Button>
+      |
+      <Button variant="text" size="none" onClick={onEdit}>
+        수정하기
+      </Button>
+      |<button onClick={onRemove}>스터디 삭제하기</button>
     </div>
   );
 }
