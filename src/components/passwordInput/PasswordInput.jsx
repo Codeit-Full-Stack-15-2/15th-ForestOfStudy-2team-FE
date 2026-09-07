2fb8e3982138119759ae9be70000c45cb98afcbb
hwang4401-sketch <hwang4401@gmail.com>
2026-09-07 15:26:07 +0900

    refactor: 비밀번호 입력 공용 컴포넌트 개선

--- src/components/passwordInput/PasswordInput.jsx
+++ src/components/passwordInput/PasswordInput.jsx
@@ -1,23 +1,53 @@
-import { useState } from 'react';
-import visibilityOn from '@/assets/ic_visibility_on.svg';
-import visibilityOff from '@/assets/ic_visibility_off.svg';
-import styles from '@/components/PasswordInput.module.css';
+import { useEffect, useRef, useState } from 'react';
+import visibilityOn from '@/assets/common/ic_visibility_on.svg';
+import visibilityOff from '@/assets/common/ic_visibility_off.svg';
+import styles from './PasswordInput.module.css';
 
-function PasswordInput({ placeholder, value, onChange, error }) {
+function PasswordInput({
+  id,
+  label = '비밀번호',
+  placeholder,
+  value,
+  onChange,
+  onKeyDown,
+  error,
+  isFocus = false,
+}) {
   const [isVisible, setIsVisible] = useState(false);
+  const passwordInputRef = useRef(null);
   const handleToggleVisibility = () => {
     setIsVisible((prev) => !prev);
   };
 
+  const isTypingInvalid = Boolean(value && value.length < 4);
+
+  const displayError =
+    error || (isTypingInvalid ? '*비밀번호는 4자 이상 입력해 주세요.' : '');
+
+  const hasError = Boolean(displayError);
+
+  useEffect(() => {
+    if (isFocus && passwordInputRef.current) {
+      passwordInputRef.current.focus();
+    }
+  }, [isFocus]);
+
   return (
     <div className={styles.wrapper}>
-      <div className={error ? styles.inputBoxError : styles.inputBox}>
+      <label className={styles.label} htmlFor={id}>
+        {label}
+      </label>
+
+      <div className={hasError ? styles.inputBoxError : styles.inputBox}>
         <input
+          ref={passwordInputRef}
+          id={id}
           className={styles.input}
           type={isVisible ? 'text' : 'password'}
           placeholder={placeholder}
           value={value}
           onChange={onChange}
+          onKeyDown={onKeyDown}
         />
 
         <button
@@ -33,7 +63,7 @@ function PasswordInput({ placeholder, value, onChange, error }) {
         </button>
       </div>
 
-      {error && <p className={styles.errorMessage}>{error}</p>}
+      {hasError && <p className={styles.errorMessage}>{displayError}</p>}
     </div>
   );
 }
