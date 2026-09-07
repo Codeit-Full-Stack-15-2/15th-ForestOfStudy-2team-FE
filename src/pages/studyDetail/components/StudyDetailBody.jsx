41a6a9e80a00f5317ed61a081c4c9f52ab866eb2
dachan <dachankim@parastar.co.kr>
2026-09-07 19:28:27 +0900

    feat: infinite scroll 적용

--- src/pages/studyDetail/components/StudyDetailBody.jsx
+++ src/pages/studyDetail/components/StudyDetailBody.jsx
@@ -1,6 +1,7 @@
 import { getStudyHabits } from '@/api/studyApi';
 import Spinner from '@/components/Spinner';
 import { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import HabitTrackerTable from './HabitTrackerTable';
 import styles from './StudyDetailBody.module.css';
 
@@ -8,6 +9,7 @@ function StudyDetailBody({ studyId }) {
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [habits, setHabits] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     const initHabits = async () => {
@@ -18,6 +20,11 @@ function StudyDetailBody({ studyId }) {
           pageSize: 7,
         });
         setHabits(data.list);
+        setPage(1);
+
+        if (data.list.length === 0 || data.list.length >= data.totalCount) {
+          setHasMore(false);
+        }
       } catch (error) {
         console.error(error.message);
       } finally {
@@ -27,13 +34,59 @@ function StudyDetailBody({ studyId }) {
     initHabits();
   }, [studyId]);
 
-  if (isLoading) <Spinner />;
+  const fetchMore = async () => {
+    const nextPage = page + 1;
+    try {
+      const data = await getStudyHabits(studyId, new Date(), {
+        page: nextPage,
+        pageSize: 7,
+      });
+
+      if (data.list.length === 0) {
+        setHasMore(false);
+        return;
+      }
+
+      setHabits((prev) => [...prev, data.list]);
+      setPage(nextPage);
+
+      if (habits.length + data.list.length >= data.totalCount) {
+        setHasMore(false);
+      }
+    } catch (error) {
+      console.error('추가 습관 데이터 로딩 실패', error.message);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div className={styles.spinnerContainer}>
+        <Spinner />
+      </div>
+    );
+  }
 
   return (
     <section className={styles.bodyContainer}>
       <h2 className={styles.title}>습관 기록표</h2>
       {habits.length > 0 ? (
-        <HabitTrackerTable habits={habits} />
+        <InfiniteScroll
+          dataLength={habits.length}
+          next={fetchMore}
+          hasMore={hasMore}
+          loader={
+            <div className={styles.loaderContainer}>
+              <Spinner size="small" />
+            </div>
+          }
+          endMessage={
+            <p className={styles.endMessage}>
+              모든 습관 데이터를 불러왔습니다.
+            </p>
+          }
+        >
+          <HabitTrackerTable habits={habits} />
+        </InfiniteScroll>
       ) : (
         <div className={styles.contentDefault}>
           <p>
