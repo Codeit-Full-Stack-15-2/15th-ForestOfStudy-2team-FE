882b44f45b56d49cc1a2537933f1a2d4ed6d01c5
dachan <dachankim@parastar.co.kr>
2026-09-08 17:57:39 +0900

    fix: 인피니티스크롤 방어로직

--- src/pages/studyDetail/components/StudyDetailBody.jsx
+++ src/pages/studyDetail/components/StudyDetailBody.jsx
@@ -57,6 +57,15 @@ function StudyDetailBody({ studyId }) {
           <Spinner />
         </div>
       )}
+      {!isLoading && !hasMore && habits.length === 0 && (
+        <div className={styles.contentDefault}>
+          <p>
+            아직 습관이 없어요
+            <br />
+            오늘의 습관에서 습관을 생성해보세요
+          </p>
+        </div>
+      )}
     </section>
   );
 }
