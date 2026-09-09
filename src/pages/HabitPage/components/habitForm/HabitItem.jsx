e2f6997d3f0fd8efb019b8c9c9f3ad5cd506b4da
YANGPA <wodud7273@gmail.com>
2026-09-09 18:01:57 +0900

    토스트라이브러리 추가

--- src/pages/HabitPage/components/habitForm/HabitItem.jsx
+++ src/pages/HabitPage/components/habitForm/HabitItem.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import styles from './HabitItem.module.css';
 import btnDeterminate from '@/assets/habitPage/btn_determinate.svg';
-import { useToast } from '@/components/toast/ToastContext';
+import { showToast } from '@/utils/showToast';
 
 function HabitItem({
   habit,
@@ -13,7 +13,7 @@ function HabitItem({
 }) {
   const [isEditing, setIsEditing] = useState(false);
   const [editHabit, setEditHabit] = useState(habit.name);
-  const { showToast } = useToast();
+
 
   useEffect(() => {
     if (isCheckMode) {
