f8b83712ad6a50b133ba1f0364f25b3677113405
hwang4401-sketch <hwang4401@gmail.com>
2026-09-10 17:17:38 +0900

    feat: StudyEdit 수정 확인 모달 추가

--- src/pages/studyEdit/StudyEdit.jsx
+++ src/pages/studyEdit/StudyEdit.jsx
@@ -1,14 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import StudyForm from '@/components/studyForm/StudyForm';
 import BaseButton from '@/components/baseButton/BaseButton';
 import styles from './StudyEdit.module.css';
 import { useStudyEditForm } from './hooks/useStudyEditForm';
 import { useNavigate, useParams } from 'react-router';
+import ConfirmModal from '@/components/confirmModal/ConfirmModal';
 import { checkIsStudyVerified } from '@/utils/studyAuthSession';
 
 function StudyEdit() {
   const navigate = useNavigate();
   const { studyId } = useParams();
+
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
 
   const {
     formData,
@@ -27,6 +30,15 @@ function StudyEdit() {
   }, [studyId, navigate]);
 
   const handleEdit = () => {
+    setIsConfirmOpen(true);
+  };
+
+  const handleCancelEdit = () => {
+    setIsConfirmOpen(false);
+  };
+
+  const handleConfirmEdit = () => {
+    setIsConfirmOpen(false);
     navigate(`/studies/${studyId}`);
   };
 
@@ -50,6 +62,15 @@ function StudyEdit() {
           </BaseButton>
         </div>
       </section>
+
+      <ConfirmModal
+        open={isConfirmOpen}
+        title="스터디를 수정하시겠습니까?"
+        cancelText="취소"
+        confirmText="확인"
+        onCancel={handleCancelEdit}
+        onConfirm={handleConfirmEdit}
+      />
     </main>
   );
 }
