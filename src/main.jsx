304126c30a0bb5c97ac4c31c36a1064a44a12de2
thcjfd <thcjfd@naver.com>
2026-09-14 18:16:40 +0900

    Revert "Update main.jsx"
    This reverts commit 288395aca89d0e5c9b3fbaad965e0f56144d975b.

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const enableMocking = async () => {
+  if (import.meta.env.VITE_USE_MSW !== 'true') {
+    return;
+  }
+  const { worker } = await import('@/mocks/browser.js');
+  return worker.start();
+};
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
