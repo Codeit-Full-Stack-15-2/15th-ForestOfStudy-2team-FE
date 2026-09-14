288395aca89d0e5c9b3fbaad965e0f56144d975b
SoCheol Kwak <thcjfd@naver.com>
2026-09-14 18:14:22 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-const enableMocking = async () => {
-  if (import.meta.env.VITE_USE_MSW !== 'true') {
-    return;
-  }
-  const { worker } = await import('@/mocks/browser.js');
-  return worker.start();
-};
-
-enableMocking().then(() => {
-  createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-});
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
